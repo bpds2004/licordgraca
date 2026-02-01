b62cb95d3c523b60582d4e1f8e871df0eaec4883
Beatriz Sousa <160726106+bpds2004@users.noreply.github.com>
2026-02-01 19:14:56 +0000

    catch

--- api/order.js
+++ api/order.js
@@ -130,7 +130,9 @@ module.exports = async (req, res) => {
     });
 
     res.status(200).json({ ok: true });
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to send email.' });
+  }  catch (error) {
+  console.error("EMAIL ERROR:", error);
+  res.status(500).json({ error: error.message });
   }
+
 };
